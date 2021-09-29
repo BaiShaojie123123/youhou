134797a0888d90a1e9882d92916eaf4c6e379710
BaiShaojie123123 <674791374@qq.com>
2021-09-29 20:45:10 +0800

    Create youhou.js

---
+++ youhou.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         test
+// @namespace    https://tampermonkey.net/
+// @version      0.1.2
+// @description  try to take over the world!
+// @author       You
+// @include http://*
+// @include https://*
+
+// @match        https://*.hyue.com/*
+// @match        http://front.hyue.com/*
+// @match        http://frontdev.hyue.com/*
+// @match        https://*.coinlist.co/*
+// @match        https://www.coinlist.co/*
+
+// @exclude       https://newassets.hcaptcha.com/*
+// @require       https://code.jquery.com/jquery-3.6.0.min.js
+
+// @updateURL     https://github.com/BaiShaojie123123/youhou/youhou.js
+
+// @icon         https://www.google.com/s2/favicons?domain=hyue.com
+// @grant        GM_openInTab
+// @grant        GM_log
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addValueChangeListener
+// @grant        window.close
+// @grant        GM_xmlhttpRequest
+// @connect      front.hyue.com
+
+
+
+// ==/UserScript==
+// @run-at document-end
+(function() {
+        var a=2;
+
+})();
+
